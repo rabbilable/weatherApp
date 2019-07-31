8077a0817ba8b624bc230780b14d6d6df47c5537
rabbilable <rabbilyasar@gmail.com>
2019-07-31 22:14:04 +0600

    finish weather app

---
+++ scripts/app.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
+
+const updateUI = (data) => {
+
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
+
+
+  // destructured properties
+  const {cityDets, weather} = data;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+      <div class="my-3">${weather.WeatherText}</div>
+      <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+  `;
+  
+  // update the night/day icon
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc)
+  icon.setAttribute('src', iconSrc);
+
+
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
+
+};
+
+const updateCity = async (city) => {
+
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+
+  return {cityDets,weather};
+  
+}
+
+form.addEventListener('submit', (e)=>{
+  // prevent default event
+  e.preventDefault();
+
+  // get city value
+  const city =form.city.value.trim();
+  form.reset();
+
+  // update the ui with new city
+  updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+
+});
