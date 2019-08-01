d7557622e2ca195dd626cdd265e1485cd0a84653
rabbilable <rabbilyasar@gmail.com>
2019-08-01 12:56:20 +0600

    implement local storage

--- scripts/app.js
+++ scripts/app.js
@@ -65,5 +65,13 @@ form.addEventListener('submit', (e)=>{
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 
+  // set local storage
+  localStorage.setItem('city', city);
 
 });
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
+}
